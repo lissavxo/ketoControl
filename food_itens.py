c250e4362abe7b21903bae0a36058509ad216f70
lissavximenes <lissavitoria12@gmail.com>
2020-05-18 09:28:55 -0300

    #addNewProperty added| food_itens

--- food_itens.py
+++ food_itens.py
@@ -1,9 +1,12 @@
 import json
 import os
+import file_handler as fh
 
 class FoodItem:
     def __init__(self):
-        self.base_food_info = './base_food_info.json'
+        self.nutrition_facts_file = './nutrition_facts.json'
+        self.properties_file = './properties.csv'
+        self.properties = fh.getCsv(self.properties_file)
 
     def create(self,item_name, params):
         # self.validade(params)
@@ -11,30 +14,73 @@ class FoodItem:
             item_name: params
         }
         data = None
-        if os.path.exists(self.base_food_info):
-            with open(self.base_food_info) as json_file:
+        if os.path.exists(self.nutrition_facts_file):
+            with open(self.nutrition_facts_file) as json_file:
                 data = json.load(json_file)
                 data.update(item)
                 #jsom.dump(data, json_file, ensure_ascii=False)
         else:
-            print ('we dont find a path', self.base_food_info)
+            print ('we dont find a path', self.nutrition_facts_file)
 
-        with open(self.base_food_info, 'w') as f:
+        with open(self.nutrition_facts_file, 'w') as f:
             json.dump(data, f, ensure_ascii=False)
-            
+    
+
+    def addPropertyByItem(self,item_name,_property):
+       
+        response = None
+        food_list = fh.getJsonKeys(self.nutrition_facts_file)
+
+        if item_name in food_list:
+
+            item = fh.getJsonObjectByKey(self.nutrition_facts_file,item_name)
+            try:
+                item[_property]
+                response = (" %s : %s " %(item[_property], _property))
+            except:
+                #print("excecao", item_name)
+                value = float(input('%s -(100g): '%_property))
+                item[_property] = value
+                new_item = {item_name:item}
+                data = fh.getUpdatedData(self.nutrition_facts_file,new_item)
+                fh.updateJson(self.nutrition_facts_file,data)
+                response = 'added'
+
+        else:
+            print("Error | food_itens | addPropertyByItem | We cant find %s in our base!" % item_name)
+        return response
+    
+    def addNewProperty(self,_property):
+        food_list = fh.getJsonKeys(self.nutrition_facts_file)
+        try:
+            for food in food_list:
+                print(food)
+                if type(_property) == list:
+                    for p in _property:
+                        response = self.addPropertyByItem(food,p)
+                else:
+                    response = self.addPropertyByItem(food,_property)
+                
+                if response:
+                    print(response)
+            fh.addCsvLine(self.properties_file,_property)
+        except:
+            print("Error | food_itens | addNewProperty ")
+
+        
     # def validateParams(self, params):
     #     if(params[""])
 
     def getItem(self, item_name, id=None):
         response = None
+        itens = fh.getJson(self.nutrition_facts_file)
+        if item_name in list(itens.keys()):
+            response = itens[item_name]
+        else:
+            print("Error | food_itens | getItem", item_name)
+            response = False
 
-        with open(self.base_food_info) as file_data:
-            itens = json.load(file_data)
-            names = list(itens.keys())
-            if item_name in itens:
-                response = itens[item_name]
-            else:
-                response = False
+       
         return response
 
     def getPropertyByItem(self,item_name,property_):
@@ -43,30 +89,18 @@ class FoodItem:
             return 0
         item = self.getItem(item_name)
         response = item[property_]
-        
-        return response 
-
-    def getCarbsByItem(self,item_name):
-        if item_name == "jejum":
-            return False
-        else:
-            return self.getPropertyByItem(item_name,"corbohydrate")
-
-    def getEnergyByItem(self,item_name):
-        if item_name == "jejum":
-            return False
-        else:
-            return self.getPropertyByItem(item_name,"energy")
-
-    def getSodiumByItem(self,item_name):
-        if item_name == "jejum":
-            return False
-        else:
-            return self.getPropertyByItem(item_name,"sodium")
+        if type(response) != int and type(response) != float:
+            print("Error | food_itens | getPropertyByItem", item_name,property_)
+        return response
 
     def formatedProperties(self):
-        return {"carbohydrate":0,"protein":0,"energy":0,"sodium":0}
+        response = {}
+        for _property in self.properties:
+            response[_property] = 0
+        return response
         
 
 
+# fi = FoodItem()
 
+# fi.addNewProperty(["lipid","fibre"])
